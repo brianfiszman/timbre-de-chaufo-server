35c43620f2316aa3971b6cb595c9862e22533f7b
FraanBat <friggio.87@gmail.com>
2024-09-01 20:23:59 -0300

    feat(storage): Obtain blobs from DB #8

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,57 @@
 import express, { Request, Response } from 'express';
 import { list } from '@vercel/blob';
 import { configDotenv } from 'dotenv';
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm"
-
-
+import { DataSource } from 'typeorm';
+import { Images } from './Image';
 
 const app = express();
-const port = process.env.PORT || 8080;
-
-// const getImages = async () => {
-//   const { blobs } = await list();
-
-//   const imagesURLs = blobs.map(blob => {
-//     return {name: blob.pathname.substring(0, blob.pathname.lastIndexOf('.webp')), url: blob.url};  
-//   });
-
-// }
+const port = process.env.PORT || 8081;
 
 configDotenv({ path: ".env.local" }); // read .env.config
 
+let dataSource: any;
+
+async function getConnection() {
+    const AppDataSource = new DataSource({
+      type: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      synchronize: false,
+      logging: true,
+      ssl: { rejectUnauthorized: false },
+      entities: [Images],
+      subscribers: [],
+      migrations: [],
+  });
+  await AppDataSource.initialize();
+  console.log("Connection has been established successfully.");
+
+  dataSource = AppDataSource;
+}
+
+getConnection().catch(error => console.error("Error de conexión: ", error));
+
+const getImages = async () => {
+  const images = await dataSource.getRepository(Images).find();
+  return images;
+}
+
+
 app.get('/', async (_req: Request, res: Response) => {
-  const { blobs } = await list();
-  return res.json(blobs);
+  return res.send("Hello World");
 })
 
-app.get('/ping', (_req: Request, res: Response) => {
-  return res.send('pong 🏓');
+app.get('/images',  async (_req: Request, res: Response) => {
+  try{
+    const response = await getImages();
+    res.send(response)
+  }
+  catch(error){
+    console.error("Error al obtener las imágenes: ", error)
+  }
 })
 
 app.listen(port, () => {
